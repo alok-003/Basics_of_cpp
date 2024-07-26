bad6bdcba1452e1d4a1a0aa4a210230ddc1a4549
alok-003 <alokchawat@gmail.com>
2024-07-26 18:09:06 +0530

    Exp_3_a_Postive_checker.cpp

--- Exp_3_a_Postive_checker.cpp
+++ Exp_3_a_Postive_checker.cpp
@@ -1,6 +1,6 @@
 //Alok Chawat
 //PRN:23070123016
-//BAtch:EnTC A-1
+//Batch:EnTC A-1
 //experiment 3.a
 //program to check if a number is positive or negative
 #include <iostream>
@@ -16,11 +16,11 @@ int main(){
         cout<<"The Number is negative";
     }
     else{
-        cout<<("The Number is Zero");
+        cout<<"The Number is Zero";
     }
 }
 /*
-**OUTPUT**
+**OUTPUT**5
 Enter a number: -90
 -90 is negative
 */
