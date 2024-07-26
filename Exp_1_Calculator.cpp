544436b0111a27ada1d188835a0bff5d8d70e23f
alok-003 <alokchawat@gmail.com>
2024-07-26 18:00:26 +0530

    Exp_1_Calculator.cpp

--- Exp_1_Calculator.cpp
+++ Exp_1_Calculator.cpp
@@ -6,19 +6,20 @@
 #include <iostream>
 using namespace std;
 int main(){
-     float division,a,b,sum,difference,product;//intialising variables
+     float div;
+     int a,b,sum,sub,mult;
      cout<<"enter first number: ";
      cin>>a;
      cout<<"enter second number: ";
      cin>>b;
-     sum = a+b;//calculate sum
-     difference = a-b;//calculate difference
-     product = a*b;//calculate product
-     division = a/b;//calculate division
-     cout<<"sum is: "<<sum<<endl;//display sum
-     cout<<"difference is: "<<difference<<endl;//display difference
-     cout<<"product is: "<<product<<endl;//display product
-     cout<<"division is: "<<division<<endl;//display division
+     sum = a+b;
+     sub = a-b;
+     multt = a*b;
+     div = a/b;
+     cout<<"sum is: "<<sum<<endl;
+     cout<<"difference is: "<<sub<<endl;
+     cout<<"product is: "<<mult<<endl;
+     cout<<"division is: "<<div<<endl;
 }
 
 /*
