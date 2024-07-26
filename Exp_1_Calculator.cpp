2618eabfc94a2f86cc813cff2861be5a0af8a685
alok-003 <alokchawat@gmail.com>
2024-07-26 17:39:43 +0530

    Add files via upload

---
+++ Exp_1_Calculator.cpp
@@ -0,0 +1,32 @@
+//Alok Chawat
+//PRN:23070123016
+//EnTC A1
+//Basic Calculator Program
+//Experiment- 1
+#include <iostream>
+using namespace std;
+int main(){
+     float division,a,b,sum,difference,product;//intialising variables
+     cout<<"enter first number: ";
+     cin>>a;
+     cout<<"enter second number: ";
+     cin>>b;
+     sum = a+b;//calculate sum
+     difference = a-b;//calculate difference
+     product = a*b;//calculate product
+     division = a/b;//calculate division
+     cout<<"sum is: "<<sum<<endl;//display sum
+     cout<<"difference is: "<<difference<<endl;//display difference
+     cout<<"product is: "<<product<<endl;//display product
+     cout<<"division is: "<<division<<endl;//display division
+}
+
+/*
+**OUTPUT**
+enter two numbers: 2 3
+sum is: 5
+difference is: -1
+product is: 6
+division is: 0.666667 
+*/
+     
