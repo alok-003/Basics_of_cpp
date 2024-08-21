2def11ea8fbd56ca9cba31577727a29fda1ec521
alok-003 <alokchawat@gmail.com>
2024-08-21 19:39:10 +0530

    Add files via upload

---
+++ Hello World.cpp
@@ -0,0 +1,17 @@
+//Name:Alok Chawat
+//PRN:23070123016
+//Aim:Printing Hello World
+
+#include <iostream>
+using namespace std;
+
+int main() { 
+    
+    cout << "Hello World";
+   
+    return 0;
+} 
+
+
+//Output:
+//Hello World
